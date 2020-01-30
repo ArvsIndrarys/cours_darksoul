405e5848eeada454b58217e74778f0724fa448eb
Arvs Indarys <marc.majewski@live.fr>
2020-01-30 17:28:14 +0100

    Removed .idea files additions -- TP continued

--- Cours_Brice/src/Point.java
+++ Cours_Brice/src/Point.java
@@ -13,6 +13,12 @@ class Point {
     private int x = -1;
     private int y;
 
+    // 4
+    private static int totalPoint;
+    private int numeroOrdre;
+
+    // 8
+    private final String nom = "PointB";
 
     // 3 - METHODE 1
     //public Point() {
@@ -28,11 +34,86 @@ class Point {
     public Point() {
     }
 
-    public Point(int x, int y) {
+//    public Point(int x, int y) {
+//        this.x=x;
+//        this.y=y;
+//    }
+
+    // 4
+//    public Point(int x, int y) {
+//        this.x=x;
+//        this.y=y;
+//        totalPoint++;
+//        numeroOrdre = totalPoint;
+//    }
+
+    // 7 C
+    private Point(int x, int y) {
         this.x=x;
         this.y=y;
+        totalPoint++;
+        numeroOrdre = totalPoint;
     }
+
+    // 7 C
+    public static Point creationPoint(int x, int y) {
+        return new Point(x, y);
+    }
+
+    // 2
+//    public String toString () {
+//        return "Point numero " + this.numeroOrdre + " sur " + Point.totalPoint + " : abscisse : " + x + ", ordonnee : " + y ;
+//    }
+
+    // 4
+//    public String toString () {
+//        return "Point numero " + this.numeroOrdre + " sur " + Point.totalPoint + " : abscisse : " + x + ", ordonnee : " + y ;
+//    }
+
+    // 8
     public String toString () {
-        return "abscisse : " + x + ", ordonnee : " + y ;
+        return "Point " + nom + " numero " + this.numeroOrdre + " sur " + Point.totalPoint + " : abscisse : " + x + ", ordonnee : " + y ;
+    }
+
+    // 5
+    public static boolean egalite(Point p1, Point p2) {
+        /* 7 A
+        *  Pas besoin d'utiliser les accesseurs car les méthondes sont déjà au sein de l'objet Point, elles ont donc un accès direct aux attributs
+         */
+        if ((p1.x == p2.x && p1.y == p2.y) || p1.numeroOrdre == p2.numeroOrdre)
+            return true;
+        else
+            return false;
+    }
+
+    // 5
+    public boolean egalite(Point p1) {
+        if ((p1.x == this.x && p1.y == this.y) || p1.numeroOrdre == this.numeroOrdre)
+            return true;
+        else
+            return false;
+    }
+
+    // 6
+    public boolean egalASoi() {
+        return egalite(this);
+    }
+
+    // 7 A
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    // 7 B
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
     }
 }

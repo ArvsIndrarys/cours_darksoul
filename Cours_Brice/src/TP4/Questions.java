f25793207841c918b32bf4fc89659f2ea9bb05ab
ArvsIndrarys <marc.majewski@live.fr>
2020-02-28 10:43:38 +0100

    Added first question

---
+++ Cours_Brice/src/TP4/Questions.java
@@ -0,0 +1,17 @@
+package TP4;
+
+public class Questions {
+
+    /*
+                                                   Object
+                                                      |
+            ------------------------------------------------------------------------------------------------
+            |           |             |               |              |                |                    |
+        Component      Math         Random          Arrays         Reader           String               Point2D
+       (abstract)     (final)         |                          (abstract)         (final)             (abstract)
+            |                         |                              |                                     |
+           List                  SecureRandom                 ------------------                         Point
+                                                              |                 |
+                                                     InputStreamReader    BufferedReader
+     */
+}
